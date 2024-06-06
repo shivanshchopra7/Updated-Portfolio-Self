2a5df660c14b11d87932845a40fccd4c68dc0281
Shivansh Chopra <gauravshivansh7@gmail.com>
2024-06-06 05:41:26 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,17 @@
+// // script.js
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const container = document.querySelector('.portfolio-container');
+//     const video = document.querySelector('.video');
+
+//     container.addEventListener('mouseenter', function() {
+//         video.style.display = 'block';
+//         video.play();
+//     });
+
+//     container.addEventListener('mouseleave', function() {
+//         video.style.display = 'none';
+//         video.pause();
+//         video.currentTime = 0; // Reset the video to the beginning
+//     });
+// });
